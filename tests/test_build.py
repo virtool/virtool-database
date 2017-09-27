bfe14641be7dbc3b1ba32bed0de83180dd4d5b2c
igboyes <igboyes@gmail.com>
2017-09-27 14:09:12 -0700

    Add test for builds

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,7 +1,12 @@
+import os
 import json
 import arrow
+import shutil
 import pytest
+import filecmp
 import subprocess
+
+TEST_FILE_PATH = "tests/files/viruses.json"
 
 
 @pytest.mark.parametrize("version", [None, "v1.0.1", "v0.8.3"])
@@ -38,3 +43,21 @@ def test_created_at(tmpdir):
     created_at = arrow.get(built_json["created_at"])
 
     assert (arrow.utcnow() - created_at).seconds == 0
+
+
+def test_divide_build(tmpdir):
+    shutil.copy(TEST_FILE_PATH, str(tmpdir))
+
+    json_path = os.path.join(str(tmpdir), "viruses.json")
+
+    src_path = os.path.join(str(tmpdir), "src")
+
+    subprocess.call(["python", "scripts/divide.py", "-o", src_path, json_path])
+
+    out_path = os.path.join(str(tmpdir), "out.json")
+
+    subprocess.call(["python", "scripts/build.py", "-f", out_path, src_path])
+
+    with open(json_path, "r") as f1:
+        with open(out_path, "r") as f2:
+            assert json.load(f1)["data"] == json.load(f2)["data"]
