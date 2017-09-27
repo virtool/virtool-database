329656f2958542f4da1bd418219dd1bf5aff4e56
igboyes <igboyes@gmail.com>
2017-09-27 11:15:26 -0700

    Clean up write_names.py

--- scripts/write_names.py
+++ scripts/write_names.py
@@ -1,12 +1,8 @@
 import json
 
-names = list()
 
 with open("viruses.json", "r") as f:    
-    names = [virus["name"] for virus in json.load(f)]
-
-
-names = sorted(names)
+    names = sorted([virus["name"] for virus in json.load(f)])
 
 with open("names.txt", "w") as f:
     f.write("\n".join(names))
